9456bc8654b051cb0b64f42d02827152ba347ee5
Gautam Kandula <164435366+GautamKandula@users.noreply.github.com>
2024-05-12 19:44:17 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('')
   
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=9239973d62a5d7cf11eb91680817687d`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid={ENTER YOUR API KEY HERE}`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
